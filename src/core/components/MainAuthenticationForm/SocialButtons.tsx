4ac2a88210623b0d35c6f6f7b3b428fe190970f1
manonja <manon.jacquin@gmail.com>
2024-02-21 14:17:38 +0100

    Small code improvements

--- src/core/components/MainAuthenticationForm/SocialButtons.tsx
+++ src/core/components/MainAuthenticationForm/SocialButtons.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { Button, ButtonProps } from "@mantine/core";
-import { TwitterIcon } from "@mantinex/dev-icons";
 import GoogleIcon from "./icons/GoogleIcon";
-
-// export function TwitterButton(props: ButtonProps & React.ComponentPropsWithoutRef<"button">) {
-//   return <Button leftIcon={<TwitterIcon />} variant="default" {...props} />;
-// }
 
 export function GoogleButton(props: ButtonProps & React.ComponentPropsWithoutRef<"button">) {
   return <Button leftIcon={<GoogleIcon />} variant="default" {...props} />;
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React from "react";
 import { BlitzPage } from "@blitzjs/next";
 import { UserInfo } from "../core/components/UserInfo";
 import Layout from "../core/layouts/Layout";
